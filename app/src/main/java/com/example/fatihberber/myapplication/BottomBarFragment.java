8d623038689f5c04ea964132a929f03ada0eacc1
Fatih BERBER <fatihberber43@gmail.com>
2018-12-13 16:12:31 +0300

    tasarım kısmı

---
+++ app/src/main/java/com/example/fatihberber/myapplication/BottomBarFragment.java
@@ -0,0 +1,43 @@
+package com.example.fatihberber.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BottomBarFragment extends Fragment {
+ protected SearchView Searchbuton;
+
+    public BottomBarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_bottom_bar, container, false);
+
+
+    Searchbuton =view.findViewById(R.id.search_bar);
+    Searchbuton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    });
+
+            return view;
+    }
+
+}
